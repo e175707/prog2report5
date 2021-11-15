197a67ba34e81b02ca0e50c8b211fe25dfdbb4f8
sakamoto yuya <e175707@ie.u-ryukyu.ac.jp>
2021-11-16 02:06:02 +0900

    1st commit

--- app/src/main/java/jp/ac/uryukyu/ie/e175707/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e175707/Enemy.java
@@ -6,13 +6,13 @@ package jp.ac.uryukyu.ie.e175707;
  *  int hitPoint; //敵のHP
  *  int attack; //敵の攻撃力
  *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
+ *  Created by e175707 on 2021/11/15.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -31,16 +31,18 @@ public class Enemy {
     /**
      * Heroへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * 攻撃する前に自身が死んでいるのなら、攻撃力を０にする。
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
         if(hitPoint>0){
+        String heroName = hero.getName();
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, heroName, damage);
         hero.wounded(damage);
         }
     }
-    
+
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
@@ -52,6 +54,70 @@ public class Enemy {
             dead = true;
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
+    }
+
+    /**
+     * 自身の名前を設定するメソッド。
+     * @param name　変更後の名前。
+     */
+    public void setName(String name){
+        this.name = name;
+    }
+
+    /**
+     * 自身のHPを設定するメソッド。
+     * @param _hitPoint 変更後のHP。
+     */
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+
+    /**
+     * 自身の攻撃力を設定するメソッド。
+     * @param attack　変更後の攻撃力。
+     */
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+
+    /**
+     * 自身の生死状態を設定するメソッド。true=死亡。
+     * @param dead　変更後の生死状態。
+     */
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+
+    /**
+     * 自身の名前を参照するメソッド。
+     * @return　自身の名前。
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * 自身のHPを参照するメソッド。
+     * @return　自身のHP。
+     */
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+
+    /**
+     * 自身の攻撃力を参照するメソッド。
+     * @return　自身の攻撃力。
+     */
+    public int getAttack(){
+        return this.attack;
+    }
+
+    /**
+     * 自身の生死状態を参照するメソッド。
+     * @return　自身の生死状態。
+     */
+    public boolean getDead(){
+        return this.dead;
     }
 
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e175707/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e175707/Hero.java
@@ -6,13 +6,13 @@ package jp.ac.uryukyu.ie.e175707;
  *  int hitPoint; //敵のHP
  *  int attack; //敵の攻撃力
  *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
+ *  Created by e175707 on 2021/11/15.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -34,8 +34,9 @@ public class Hero {
      * @param e 攻撃対象
      */
     public void attack(Enemy e){
+        String eName = e.getName();
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, eName, damage);
         e.wounded(damage);
     }
 
@@ -50,5 +51,69 @@ public class Hero {
             dead = true;
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
         }
+    }
+
+    /**
+     * 自身の名前を設定するメソッド。
+     * @param name　変更後の名前。
+     */
+    public void setName(String name){
+        this.name = name;
+    }
+
+    /**
+     * 自身のHPを設定するメソッド。
+     * @param hitPoint　変更後のHP。
+     */
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+
+    /**
+     * 自身の攻撃力を設定するメソッド。
+     * @param attack　変更後の攻撃力。
+     */
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+
+    /**
+     * 自身の生死状態を設定するメソッド。
+     * @param dead　変更後の生死状態。
+     */
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+
+    /**
+     * 自身の名前を参照するメソッド。
+     * @return　自身の名前。
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * 自身のHPを参照するメソッド。
+     * @return　自身のHP。
+     */
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+
+    /**
+     * 自身の攻撃力を参照するメソッド。
+     * @return　自身の攻撃力。
+     */
+    public int getAttack(){
+        return this.attack;
+    }
+
+    /**
+     * 自身の生死状態を参照するメソッド。
+     * @return　自身の生死状態。
+     */
+    public boolean getDead(){
+        return this.dead;
     }
 }
