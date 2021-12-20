076a5404db88cfa15f8c28fd5bcc43ba34b30411
sakamoto yuya <e175707@ie.u-ryukyu.ac.jp>
2021-12-20 21:25:38 +0900

    add new file

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,22 +1,6 @@
-import jp.ac.uryukyu.ie.e175707.*;
-
 public class Main {
-    public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
-        Enemy enemy = new Enemy("スライム", 6, 3);
-
-        String heroName = hero.getName();
-        String enemyName = enemy.getName();
-
-        System.out.printf("%s vs. %s\n", heroName, enemyName);
-
-        int turn = 0;
-        while( hero.getDead() == false && enemy.getDead() == false ){
-            turn++;
-            System.out.printf("%dターン目開始！\n", turn);
-            hero.attack(enemy);
-            enemy.attack(hero);
-        }
-        System.out.println("戦闘終了");
+    public static void main(String[] args) throws NumberFormatException {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
